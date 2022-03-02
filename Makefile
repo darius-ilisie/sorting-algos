487f09a08c05d5a85f562874c6342fc4ec2da8ad
Ilisie Darius-Octavian <91126656+darius-ilisie@users.noreply.github.com>
2022-03-02 18:44:25 +0200

    Added random array gen (python3)

---
+++ Makefile
@@ -0,0 +1,13 @@
+C = gcc
+OPT = -O3 -march=haswell -D
+EXE = sort.
+
+run:
+	python3 scripts/random.py > bin/tmp
+	cd bin/
+	./$(EXE).QUICK
+
+	cd ../
+
+build:
+	$(C) $(OPT)QUICK src/sort.c -o bin/$(EXE).QUICK
---
+++ scripts/random.py
@@ -0,0 +1,13 @@
+from random import seed
+from random import random
+
+LARGEST_NUMBER = 2147483647
+NUMBERS = 1000000
+
+with open("/dev/urandom", "rb") as f:
+    s_bytes = f.read(4)
+seed(s_bytes)
+# generate some random numbers
+print(f"{NUMBERS}", end=' ')
+for i in range(NUMBERS):
+    print("{}".format(int(random()*10000000000) % (LARGEST_NUMBER + 1)), end=' ')
