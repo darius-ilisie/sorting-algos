e1fa86addeb6dc0382b3a59dab9f288353d24ce4
Ilisie Darius-Octavian <91126656+darius-ilisie@users.noreply.github.com>
2022-03-02 18:52:07 +0200

    Fixed python3 error

--- Makefile
+++ Makefile
@@ -3,11 +3,11 @@ OPT = -O3 -march=haswell -D
 EXE = sort.
 
 run:
-	python3 scripts/random.py > bin/tmp
+	python3 scripts/random_gen.py > bin/tmp
 	cd bin/
 	./$(EXE).QUICK
 
 	cd ../
 
 build:
-	$(C) $(OPT)QUICK src/sort.c -o bin/$(EXE).QUICK
+	$(C) $(OPT)QUICK src/sort.c -o bin/$(EXE)QUICK
--- scripts/random_gen.py
+++ scripts/random_gen.py
@@ -1,13 +1,12 @@
-from random import seed
-from random import random
+import random
 
 LARGEST_NUMBER = 2147483647
 NUMBERS = 1000000
 
 with open("/dev/urandom", "rb") as f:
     s_bytes = f.read(4)
-seed(s_bytes)
+random.seed(s_bytes)
 # generate some random numbers
 print(f"{NUMBERS}", end=' ')
 for i in range(NUMBERS):
-    print("{}".format(int(random()*10000000000) % (LARGEST_NUMBER + 1)), end=' ')
+    print("{}".format(int(random.random()*10000000000) % (LARGEST_NUMBER + 1)), end=' ')
